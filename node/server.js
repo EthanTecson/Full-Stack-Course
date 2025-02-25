e9f102ea2228c37420971c8cc0b0b0a59eeac602
EthanTecson <ethantecson@gmail.com>
2025-02-25 16:22:13 -0500

    index.js removed from configs

--- node/server.js
+++ node/server.js
@@ -92,7 +92,7 @@ app.use(unknownEndpoint)
 app.use(express.json())
 app.use(requestLogger)
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
