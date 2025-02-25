c1bdf94d319aa99b67cc6a965302102ccd0cf384
EthanTecson <ethantecson@gmail.com>
2025-02-25 14:39:25 -0500

    CORS added and bug fixed. Fuq dis shi

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -15,9 +15,25 @@ const App = () => {
     noteService
       .getAll()
       .then(response => {
-        setNotes(response.data)
+        setNotes(response)
       })
   }, [])
+
+
+  const addNote = (event) => {
+    event.preventDefault()
+    const noteObject = {
+      content: newNote,
+      important: Math.random() < 0.5,
+    }
+
+    noteService
+      .create(noteObject)
+      .then((returnedNote) => {
+        setNotes(notes.concat(returnedNote))
+        setNewNote('')
+      })
+  }
 
   const toggleImportanceOf = id => {
     const note = notes.find(n => n.id === id)
@@ -26,7 +42,7 @@ const App = () => {
     noteService
       .update(id, changedNote)
       .then(response => {
-        setNotes(notes.map(note => note.id === id ? response.data : note))
+        setNotes(notes.map((note) => (note.id === id ? response.data : note)))
       })
       .catch(error => {
         setErrorMessage(
@@ -39,27 +55,11 @@ const App = () => {
       })
   }
 
-  const addNote = event => {
-    event.preventDefault()
-    const noteObject = {
-      content: newNote,
-      important: Math.random() < 0.5,
-    }
-
-    noteService
-      .create(noteObject)
-      .then(response => {
-        setNotes(notes.concat(response.data))
-        setNewNote('')
-      })
-  }
-
   const handleNoteChange = (event) => {
-    console.log(event.target.value)
     setNewNote(event.target.value)
   }
 
-  const notesToShow = showAll ? notes : notes.filter(note => note.important)
+  const notesToShow = showAll ? notes : notes.filter((note) => note.important)
 
   return (
     <div>
