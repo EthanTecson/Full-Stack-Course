6e064b62a6be15d5dd28975a375625dd1bbee7a5
EthanTecson <ethantecson@gmail.com>
2025-03-04 15:30:29 -0500

    tests are passing

--- backend/tests/note_api.test.js
+++ backend/tests/note_api.test.js
@@ -1,28 +1,31 @@
-const { test, after } = require('node:test')
+const { test, after, describe } = require('node:test')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
+const assert = require('node:assert')
 
 const api = supertest(app)
 
-test('notes are returned as josn', async () => {
-    await api
-        .get('/api/notes')
-        .expect(200)
-        .expect('Content-Type', /application\/json/)
-})
+describe('api tests', () => {
+    test('notes are returned as josn', async () => {
+        await api
+            .get('/api/notes')
+            .expect(200)
+            .expect('Content-Type', /application\/json/)
+    })
 
-test('there are two notes', async () => {
-    const response = await api.get('/api/notes')
+    test('there are two notes', async () => {
+        const response = await api.get('/api/notes')
 
-    assert.strictEqual(response.body.length, 2)
-})
+        assert.strictEqual(response.body.length, 2)
+    })
 
-test('the first note is about HTTP methods', async () => {
-    const response = await api.get('/api/notes')
+    test('the first note is about HTTP methods', async () => {
+        const response = await api.get('/api/notes')
 
-    const contents = response.body.map(e => e.content)
-    assert(contents.includes('HTML is easy'))
+        const contents = response.body.map(e => e.content)
+        assert(contents.includes('HTML is easy'))
+    })
 })
 
 after(async () => {
